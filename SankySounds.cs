64e55ed59a1870f43b3d0add76a57f8416ae73c8
T3Marius <168598779+T3Marius@users.noreply.github.com>
2024-07-26 23:09:33 +0300

    Add files via upload

--- SankySounds.cs
+++ SankySounds.cs
@@ -2,7 +2,6 @@
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API;
-using CounterStrikeSharp.API.Modules.Utils;
 using System.Text.Json.Serialization;
 using CounterStrikeSharp.API.Core.Translations;
 
@@ -24,7 +23,10 @@ namespace SankySounds
         public override string ModuleVersion => "0.0.4";
 
         public PluginConfig Config { get; set; } = new PluginConfig();
+
         public Dictionary<int?, DateTime> lastCommandUsage = new Dictionary<int?, DateTime>();
+        public Dictionary<int?, bool> PlayerSoundSettings { get; set; } = new Dictionary<int?, bool>();
+
 
         public void OnConfigParsed(PluginConfig config)
         {
@@ -49,34 +51,80 @@ namespace SankySounds
             if (player == null || !player.IsValid || player.IsBot)
                 return HookResult.Continue;
 
+            if (info.ArgByIndex(1)?.Equals("!sounds", StringComparison.OrdinalIgnoreCase) == true)
+            {
+                bool newSetting;
+
+                if (PlayerSoundSettings.ContainsKey(player.UserId))
+                {
+                    newSetting = !PlayerSoundSettings[player.UserId];
+                }
+                else
+                {
+                    newSetting = false;
+                }
+
+                // Update player sound setting
+                PlayerSoundSettings[player.UserId] = newSetting;
+
+                // Inform the player of the change
+                string statusMessage = newSetting
+                    ? Localizer["command.sound.enabled"]
+                    : Localizer["command.sound.disabled"];
+
+                string message = $"{Config.Tag} {statusMessage}";
+                player.PrintToChat(message);
+
+                return HookResult.Handled;
+            }
+
             if (Config.Permission != "" && !AdminManager.PlayerHasPermissions(player, $"{Config.Permission}"))
+            {
+                string message = $"{Config.Tag} {Localizer["no_permission"]}";
+                player.PrintToChat(message);
                 return HookResult.Continue;
+            }
 
             DateTime now = DateTime.Now;
 
-            foreach (var sound in Config.Sounds)
+            string commandArgument = info.ArgByIndex(1);
+
+            // Check if the command starts with a period
+            if (commandArgument != null && commandArgument.StartsWith("."))
             {
-                if (info.ArgByIndex(1) == sound.Key)
+                string soundKey = commandArgument.Substring(1);
+
+                if (Config.Sounds.TryGetValue(soundKey, out string soundValue))
                 {
                     if (Config.CommandsCooldown > 0 && lastCommandUsage.TryGetValue(player.UserId, out DateTime lastUsage))
                     {
                         TimeSpan cooldownTime = now - lastUsage;
                         if (cooldownTime.TotalSeconds < Config.CommandsCooldown)
                         {
-                            player.PrintToChat(Config.Tag + Localizer["Cooldown"]);
+                            int remainingSeconds = (int)Math.Ceiling(Config.CommandsCooldown - cooldownTime.TotalSeconds);
+                            string message = $"{Config.Tag} {Localizer["cooldown"]}";
+                            message = message.Replace("{secondsRemaining}", remainingSeconds.ToString());
+                            player.PrintToChat(message);
                             return HookResult.Continue;
                         }
                     }
-                    Utilities.GetPlayers().ForEach(player =>
+
+                    // Check if the player has disabled sounds
+                    if (PlayerSoundSettings.GetValueOrDefault(player.UserId, true) == false)
                     {
-                        if (player is { IsValid: true })
+                        return HookResult.Continue;
+                    }
+
+                    Utilities.GetPlayers().ForEach(p =>
+                    {
+                        if (p is { IsValid: true } && PlayerSoundSettings.GetValueOrDefault(p.UserId, true))
                         {
-                            player.ExecuteClientCommand($"play {sound.Value}");
+                            p.ExecuteClientCommand($"play {soundValue}");
                         }
                     });
+
                     lastCommandUsage[player.UserId] = now;
                     return Config.ShowCommandInChat ? HookResult.Continue : HookResult.Handled;
-
                 }
             }
 

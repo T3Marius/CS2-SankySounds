29e6c3a39640dbd12bc207e64e6c0f52328ddff3
T3Marius <168598779+T3Marius@users.noreply.github.com>
2024-10-12 01:27:00 +0300

    Add files via upload

---
+++ src/Config/Config.cs
@@ -0,0 +1,83 @@
+﻿using CounterStrikeSharp.API;
+using System.Reflection;
+using Tomlyn.Model;
+using Tomlyn;
+
+namespace SankySounds
+{
+    public static class Config_Config
+    {
+        public static Cfg Config { get; set; } = new Cfg();
+
+        public static void Load()
+        {
+            string assemblyName = Assembly.GetExecutingAssembly().GetName().Name ?? "";
+            string cfgPath = $"{Server.GameDirectory}/csgo/addons/counterstrikesharp/configs/plugins/{assemblyName}";
+
+            LoadConfig($"{cfgPath}/config.toml");
+        }
+
+        private static void LoadConfig(string configPath)
+        {
+            if (!File.Exists(configPath))
+            {
+                throw new FileNotFoundException($"Configuration file not found: {configPath}");
+            }
+
+            string configText = File.ReadAllText(configPath);
+            TomlTable model = Toml.ToModel(configText);
+
+            TomlTable soundsTable = (TomlTable)model["Sounds"];
+            Dictionary<string, string> soundsDict = soundsTable.ToDictionary(
+                kvp => kvp.Key,
+                kvp => kvp.Value.ToString()!
+            );
+
+            TomlTable permissionsTable = (TomlTable)model["Permissions"];
+            List<string> permissionsList = new();
+            foreach (var permission in (TomlArray)permissionsTable["Permissions"])
+            {
+                permissionsList.Add(permission!.ToString()!);
+            }
+
+            TomlTable settingsTable = (TomlTable)model["Settings"];
+            Config_Settings config_settings = new()
+            {
+                CommandsCooldown = int.Parse(settingsTable["CommandsCooldown"]?.ToString() ?? "0"),
+                SoundsPrefix = settingsTable["SoundsPrefix"]?.ToString() ?? ".",
+                EnableMenu = bool.Parse(settingsTable["EnableMenu"].ToString()!),
+                SankyMenu = GetTomlArray(settingsTable, "SankyMenu")
+            };
+
+            Config = new Cfg
+            {
+                Sounds = soundsDict,
+                Permissions = permissionsList,
+                Settings = config_settings
+            };
+        }
+        private static string[] GetTomlArray(TomlTable table, string key)
+        {
+            if (table.TryGetValue(key, out var value) && value is TomlArray array)
+            {
+                return array.OfType<string>().ToArray();
+            }
+            return Array.Empty<string>();
+        }
+
+        public class Cfg
+        {
+            public Dictionary<string, string> Sounds { get; set; } = new();
+            public List<string> Permissions { get; set; } = new();
+            public Config_Settings Settings { get; set; } = new();
+        }
+
+        public class Config_Settings
+        {
+            public int CommandsCooldown { get; set; }
+            public string SoundsPrefix { get; set; } = ".";
+            public bool EnableMenu { get; set; } = false;
+            public string[] SankyMenu { get; set; } = Array.Empty<string>();
+        }
+    }
+}
---
+++ src/SankySounds.cs
@@ -0,0 +1,64 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Commands;
+using static SankySounds.Config_Config;
+
+namespace SankySounds;
+
+public class SankySounds : BasePlugin
+{
+    public override string ModuleAuthor => "T3Marius";
+    public override string ModuleName => "SankySounds";
+    public override string ModuleVersion => "1.0";
+    public static SankySounds Instance { get; set; } = new SankySounds();
+
+    public static Dictionary<int, DateTime> LastCommandUsage { get; set; } = new Dictionary<int, DateTime>();
+    public override void Load(bool hotReload)
+    {
+        Instance = this;
+        Menu.Load();
+        AddCommandListener("say", Command_Say, HookMode.Pre);
+        AddCommandListener("say_team", Command_Say, HookMode.Pre);
+        Config_Config.Load();
+    }
+    public HookResult Command_Say(CCSPlayerController? player, CommandInfo info)
+    {
+        if (player == null || !player.IsValid || player.IsBot)
+            return HookResult.Continue;
+
+        string commandArgument = info.ArgByIndex(1);
+        string prefix = Config.Settings.SoundsPrefix;
+
+        if (Config.Permissions.Count > 0 && !Config.Permissions.Any(permission => AdminManager.PlayerHasPermissions(player, permission)))
+        {
+            return HookResult.Continue;
+        }
+
+        if (commandArgument != null && commandArgument.StartsWith(prefix))
+        {
+            string soundKey = commandArgument.Substring(prefix.Length);
+
+            if (Config.Sounds.TryGetValue(soundKey, out var sound))
+            {
+                DateTime now = DateTime.Now;
+
+                if (LastCommandUsage.TryGetValue(player.UserId!.Value, out DateTime lastUsage) &&
+                    (now - lastUsage).TotalSeconds < Config.Settings.CommandsCooldown)
+                {
+                    int remainingSeconds = (int)(Config.Settings.CommandsCooldown - (now - lastUsage).TotalSeconds);
+                    player.PrintToChat(Localizer["prefix"] + Localizer["cooldown", remainingSeconds]);
+                    return HookResult.Continue;
+                }
+
+                LastCommandUsage[player.UserId!.Value] = now;
+
+                Utilities.GetPlayers().ForEach(p =>
+                {
+                    p.ExecuteClientCommand($"play {sound}");
+                });
+            }
+        }
+        return HookResult.Continue;
+    }
+}
